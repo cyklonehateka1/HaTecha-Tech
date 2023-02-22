bdf7e7dc32e597c2afbbaaea88be382a07449ba9
Cyklone Hateka <emmanuelhateka1@gmail.com>
2023-02-22 14:47:01 +0000

    started with hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 const Home = () => {
   return (
     <div className="w-screen">
       <Navbar />
+      <Hero />
     </div>
   );
 };
