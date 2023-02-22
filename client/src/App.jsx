8ee61493f4b0ce5eaedecfe7a05eabd667dba2d5
Cyklone Hateka <emmanuelhateka1@gmail.com>
2023-02-22 01:20:38 +0000

    setting up dev environment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
@@ -28,7 +25,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
