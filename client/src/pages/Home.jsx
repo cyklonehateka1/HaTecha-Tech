b9d4ffffc76f7e04d257b5004bf6af0c92bb74b0
Cyklone Hateka <emmanuelhateka1@gmail.com>
2023-02-26 11:11:01 +0000

    Services section in progress

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import "../styles/pages/home.css";
+import Services from "../components/Services";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   );
 };
