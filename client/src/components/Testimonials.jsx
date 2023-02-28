735804ea31f96791abff5413eef8907ff8af8049
Cyklone Hateka <emmanuelhateka1@gmail.com>
2023-02-28 14:22:57 +0000

    just started with testimonials section design

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,5 +1,67 @@
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="testimonials">
+      <div className="testimonialsCont">
+        <h3>We leave the testimonies to our happy clients and affliates</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Necessitatibus laboriosam non numquam esse odio blanditiis fugiat
+          deserunt suscipit ut ex corporis repudiandae, nostrum vitae eos
+          excepturi praesentium autem voluptatem quisquam.
+        </p>
+        <div className="clientsCont">
+          <div className="">
+            <div className="clientImageDetCont">
+              <img src="" alt="" />
+              <div className="details">
+                <h5></h5>
+                <p></p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+          <div className="">
+            <div className="clientImageDetCont">
+              <img src="" alt="" />
+              <div className="details">
+                <h5></h5>
+                <p></p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+          <div className="">
+            <div className="clientImageDetCont">
+              <img src="" alt="" />
+              <div className="details">
+                <h5></h5>
+                <p></p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+          <div className="">
+            <div className="clientImageDetCont">
+              <img src="" alt="" />
+              <div className="details">
+                <h5></h5>
+                <p></p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
